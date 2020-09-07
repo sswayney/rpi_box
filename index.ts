83a506f3ce53bd8903c85cabdc51638244d93eae
sswayney <shane.swayney@gmail.com>
2020-09-06 17:20:25 -0700

    latch switch - like a button

--- index.ts
+++ index.ts
@@ -2,8 +2,17 @@ import * as gpio from 'rpi-gpio';
 
 const pin = 12;
 
-gpio.on('change', (channel, value) => {
-console.log('Channel ' + channel + ' value is now ' + value);
-});
+gpio.on('change', channelValueListener());
 
 gpio.setup(pin, gpio.DIR_IN, gpio.EDGE_BOTH);
+
+function channelValueListener(): (...args: any[]) => void {
+    let lastValue: boolean | undefined = undefined;
+    return (channel, value) => {
+        if (lastValue !== value){
+            lastValue = value;
+            console.log('Channel ' + channel + ' value is now ' + value);
+        }
+    }
+        
+}
