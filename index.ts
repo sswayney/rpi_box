cb91bcefeba2e9471450871ae57870c7b586d19a
Shane Swayney <sswayney@conversantmedia.com>
2020-09-06 18:43:17 -0700

    wip led

--- index.ts
+++ index.ts
@@ -1,20 +1,47 @@
 import * as gpio from 'rpi-gpio';
 
-const pin12 = 12;
-const pin16 = 16;
+/**
+ * Output led
+ */
+enum OutputPins {
+    pin7_led1 = 7
+}
 
+gpio.setup(OutputPins.pin7_led1, gpio.DIR_OUT);
+
+/**
+ * Input switches
+ */
+enum InputPins {
+    pin12_switch1 = 12,
+    pin16_switch2 = 16
+}
+
+gpio.setup(InputPins.pin12_switch1, gpio.DIR_IN, gpio.EDGE_BOTH);
+gpio.setup(InputPins.pin16_switch2, gpio.DIR_IN, gpio.EDGE_BOTH);
+
+/**
+ * Value change listener
+ */
 gpio.on('change', channelValueListener());
 
-gpio.setup(pin12, gpio.DIR_IN, gpio.EDGE_BOTH);
-gpio.setup(pin16, gpio.DIR_IN, gpio.EDGE_BOTH);
 
 function channelValueListener(): (...args: any[]) => void {
-    const lastValues: Map<any,any> = new Map();
+    const lastValues: Map<any, any> = new Map();
     return (channel, value) => {
-        if (lastValues.get(channel) !== value){
-            lastValues.set(channel,value);
+        if (lastValues.get(channel) !== value) {
+            lastValues.set(channel, value);
             console.log('Channel ' + channel + ' value is now ' + value);
+
+            switch (channel) {
+                case InputPins.pin12_switch1:
+                    console.log(`Writting ${value} to ${OutputPins.pin7_led1}`);
+                    gpio.write(OutputPins.pin7_led1, value);
+                    break;
+                case InputPins.pin16_switch2:
+                    break;
+            }
         }
     }
-        
+
 }
