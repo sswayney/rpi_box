a5098cb6c371480f770a361e1f8695143af2514d
Shane Swayney <sswayney@conversantmedia.com>
2020-09-08 18:34:42 -0700

    tm1637

---
+++ libs/tm1637.ts
@@ -0,0 +1,278 @@
+import * as gpio from "rpi-gpio";
+
+//
+//      A
+//     ---
+//  F |   | B
+//     -G-
+//  E |   | C
+//     ---
+//      D
+const allowedChars=['0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'A',
+    'a',
+    'B',
+    'b',
+    'C',
+    'c',
+    'D',
+    'd',
+    'E',
+    'e',
+    'F',
+    'f',
+    'G',
+    'g',
+    'H',
+    'h',
+    'I',
+    'i',
+    'J',
+    'j',
+    'K',
+    'k',
+    'L',
+    'l',
+    'M',
+    'm',
+    'N',
+    'n',
+    'O',
+    'o',
+    'P',
+    'p',
+    'Q',
+    'q',
+    'R',
+    'r',
+    'S',
+    's',
+    'T',
+    't',
+    'U',
+    'u',
+    'V',
+    'v',
+    'W',
+    'w',
+    'X',
+    'x',
+    'Y',
+    'y',
+    'Z',
+    'z',
+    ' ',
+    '°',
+    '-',
+    '=',
+    '[',
+    ']',
+    '(',
+    ')'];
+
+const codigitToSegment = [
+    // XGFEDCBA
+    0b00111111, // 0
+    0b00000110, // 1
+    0b01011011, // 2
+    0b01001111, // 3
+    0b01100110, // 4
+    0b01101101, // 5
+    0b01111101, // 6
+    0b00000111, // 7
+    0b01111111, // 8
+    0b01101111, // 9
+    0b01110111, // A
+    0b01011111, // a
+    0b01111111, // B
+    0b01111100, // b
+    0b00111001, // C
+    0b01011000, // c
+    0b00111111, // D
+    0b01011110, // d
+    0b01111001, // E
+    0b01111011, // e
+    0b01110001, // F
+    0b01110001, // f
+    0b00111101, // G
+    0b01101111, // g
+    0b01110110, // H
+    0b01110100, // h
+    0b00000110, // I
+    0b00000100, // i
+    0b00011110, // J
+    0b00001110, // j
+    0b01110110, // K
+    0b01110100, // k
+    0b00111000, // L
+    0b00000110, // l
+    0b00110111, // M
+    0b01010100, // m
+    0b01110110, // N
+    0b01010100, // n
+    0b00111111, // O
+    0b01011100, // o
+    0b01110011, // P
+    0b01110011, // p
+    0b01100111, // Q
+    0b01100111, // q
+    0b01110111, // R
+    0b01010000, // r
+    0b01101101, // S
+    0b01101101, // s
+    0b01111000, // T
+    0b01111000, // t
+    0b00111110, // U
+    0b00011100, // u
+    0b00111110, // V
+    0b00011100, // v
+    0b00111110, // W
+    0b00011100, // w
+    0b01110110, // X
+    0b01110110, // x
+    0b01100110, // Y
+    0b01100110, // y
+    0b01011011, // z
+    0b01011011, // Z
+    0b00000000, // " "
+    0b01100011, // °
+    0b01000000, // -
+    0b01001000, // =
+    0b00111001, // (
+    0b00001111, // )
+    0b00111001, // [
+    0b00001111  // ]
+];
+
+const sleep = () => new Promise((r) => setTimeout(r, 1));
+
+export class TM1637 {
+    _text: string;
+    _split: boolean;
+    _alignLeft: boolean;
+
+    constructor(protected pinClk, protected pinDIO) {
+
+        this._text = '';
+        this._split = false;
+        this._alignLeft = false;
+        gpio.setup(this.pinClk, gpio.DIR_OUT);
+        gpio.setup(this.pinDIO, gpio.DIR_OUT);
+        gpio.write(this.pinClk, true);
+        gpio.write(this.pinDIO, true);
+    }
+
+    high(pin) {
+        gpio.write(pin, true);
+        sleep();
+    }
+
+    low(pin) {
+        gpio.write(pin, false);
+        sleep();
+    }
+
+    start() {
+        this.low(this.pinDIO);
+    }
+
+    writeBit(value) {
+        this.low(this.pinClk);
+        if (value)
+            this.high(this.pinDIO);
+        else
+            this.low(this.pinDIO);
+
+        this.high(this.pinClk);
+    }
+    readAck() {
+        this.low(this.pinClk);
+        // gpio.setup(this.pinDIO, gpio.DIR_IN);
+        this.high(this.pinClk);
+        // const ack = gpio.promise.read(this.pinDIO);
+        //gpio.setup(this.pinDIO, gpio.DIR_OUT);
+        this.low(this.pinClk);
+        // return ack;
+    }
+
+    writeByte(byte) {
+        let b = byte;
+        for (let i = 0; i < 8; i++) {
+            this.writeBit(b & 0x01);
+            b >>= 1;
+        }
+        return this.readAck();
+    }
+
+    stop() {
+        this.low(this.pinDIO);
+        this.high(this.pinClk);
+        this.high(this.pinDIO);
+    }
+
+    set text(message){
+        this._text = (message+"").substring(0,4);
+        this.sendData();
+    }
+
+    get text(){
+        return this._text;
+    }
+
+    set split(value){
+        this._split = value === true;
+        this.sendData();
+    }
+
+    get split(){
+        return this._split;
+    }
+
+    set alignLeft(value){
+        this._alignLeft = value === true;
+        this.sendData();
+    }
+
+    get alignLeft(){
+        return this._alignLeft;
+    }
+
+    sendData() {
+        let m=[null,null,null,null];
+        for (let i = this._text.length; i >= 0 ; i--) {
+            let ind = allowedChars.indexOf(this._text[i]);
+            if(ind>-1)
+                if (!this._alignLeft) {
+                    m[(4-this._text.length)+i]=ind;
+                } else {
+                    m[i]=ind;
+                }
+        }
+        let numsEncoded = [0, 0, 0, 0].map((u, i) => codigitToSegment[m[i]] || 0);
+        if (this._split) numsEncoded[1] = numsEncoded[1] | 0b10000000;
+
+        this.start();
+        this.writeByte(0b01000000);
+        this.stop();
+
+        this.start();
+        this.writeByte(0b11000000);
+        for (let i = 0; i < numsEncoded.length; i++) {
+            this.writeByte(numsEncoded[i]);
+        }
+        this.stop();
+
+        this.start();
+        this.writeByte(0b10001111);
+        this.stop();
+    }
+}
---
+++ out/libs/tm1637.js
@@ -0,0 +1,273 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var gpio = require("rpi-gpio");
+//
+//      A
+//     ---
+//  F |   | B
+//     -G-
+//  E |   | C
+//     ---
+//      D
+var allowedChars = ['0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'A',
+    'a',
+    'B',
+    'b',
+    'C',
+    'c',
+    'D',
+    'd',
+    'E',
+    'e',
+    'F',
+    'f',
+    'G',
+    'g',
+    'H',
+    'h',
+    'I',
+    'i',
+    'J',
+    'j',
+    'K',
+    'k',
+    'L',
+    'l',
+    'M',
+    'm',
+    'N',
+    'n',
+    'O',
+    'o',
+    'P',
+    'p',
+    'Q',
+    'q',
+    'R',
+    'r',
+    'S',
+    's',
+    'T',
+    't',
+    'U',
+    'u',
+    'V',
+    'v',
+    'W',
+    'w',
+    'X',
+    'x',
+    'Y',
+    'y',
+    'Z',
+    'z',
+    ' ',
+    '°',
+    '-',
+    '=',
+    '[',
+    ']',
+    '(',
+    ')'];
+var codigitToSegment = [
+    // XGFEDCBA
+    63,
+    6,
+    91,
+    79,
+    102,
+    109,
+    125,
+    7,
+    127,
+    111,
+    119,
+    95,
+    127,
+    124,
+    57,
+    88,
+    63,
+    94,
+    121,
+    123,
+    113,
+    113,
+    61,
+    111,
+    118,
+    116,
+    6,
+    4,
+    30,
+    14,
+    118,
+    116,
+    56,
+    6,
+    55,
+    84,
+    118,
+    84,
+    63,
+    92,
+    115,
+    115,
+    103,
+    103,
+    119,
+    80,
+    109,
+    109,
+    120,
+    120,
+    62,
+    28,
+    62,
+    28,
+    62,
+    28,
+    118,
+    118,
+    102,
+    102,
+    91,
+    91,
+    0,
+    99,
+    64,
+    72,
+    57,
+    15,
+    57,
+    15 // ]
+];
+var sleep = function () { return new Promise(function (r) { return setTimeout(r, 1); }); };
+var TM1637 = /** @class */ (function () {
+    function TM1637(pinClk, pinDIO) {
+        this.pinClk = pinClk;
+        this.pinDIO = pinDIO;
+        this._text = '';
+        this._split = false;
+        this._alignLeft = false;
+        gpio.setup(this.pinClk, gpio.DIR_OUT);
+        gpio.setup(this.pinDIO, gpio.DIR_OUT);
+        gpio.write(this.pinClk, true);
+        gpio.write(this.pinDIO, true);
+    }
+    TM1637.prototype.high = function (pin) {
+        gpio.write(pin, true);
+        sleep();
+    };
+    TM1637.prototype.low = function (pin) {
+        gpio.write(pin, false);
+        sleep();
+    };
+    TM1637.prototype.start = function () {
+        this.low(this.pinDIO);
+    };
+    TM1637.prototype.writeBit = function (value) {
+        this.low(this.pinClk);
+        if (value)
+            this.high(this.pinDIO);
+        else
+            this.low(this.pinDIO);
+        this.high(this.pinClk);
+    };
+    TM1637.prototype.readAck = function () {
+        this.low(this.pinClk);
+        // gpio.setup(this.pinDIO, gpio.DIR_IN);
+        this.high(this.pinClk);
+        // const ack = gpio.promise.read(this.pinDIO);
+        //gpio.setup(this.pinDIO, gpio.DIR_OUT);
+        this.low(this.pinClk);
+        // return ack;
+    };
+    TM1637.prototype.writeByte = function (byte) {
+        var b = byte;
+        for (var i = 0; i < 8; i++) {
+            this.writeBit(b & 0x01);
+            b >>= 1;
+        }
+        return this.readAck();
+    };
+    TM1637.prototype.stop = function () {
+        this.low(this.pinDIO);
+        this.high(this.pinClk);
+        this.high(this.pinDIO);
+    };
+    Object.defineProperty(TM1637.prototype, "text", {
+        get: function () {
+            return this._text;
+        },
+        set: function (message) {
+            this._text = (message + "").substring(0, 4);
+            this.sendData();
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(TM1637.prototype, "split", {
+        get: function () {
+            return this._split;
+        },
+        set: function (value) {
+            this._split = value === true;
+            this.sendData();
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(TM1637.prototype, "alignLeft", {
+        get: function () {
+            return this._alignLeft;
+        },
+        set: function (value) {
+            this._alignLeft = value === true;
+            this.sendData();
+        },
+        enumerable: true,
+        configurable: true
+    });
+    TM1637.prototype.sendData = function () {
+        var m = [null, null, null, null];
+        for (var i = this._text.length; i >= 0; i--) {
+            var ind = allowedChars.indexOf(this._text[i]);
+            if (ind > -1)
+                if (!this._alignLeft) {
+                    m[(4 - this._text.length) + i] = ind;
+                }
+                else {
+                    m[i] = ind;
+                }
+        }
+        var numsEncoded = [0, 0, 0, 0].map(function (u, i) { return codigitToSegment[m[i]] || 0; });
+        if (this._split)
+            numsEncoded[1] = numsEncoded[1] | 128;
+        this.start();
+        this.writeByte(64);
+        this.stop();
+        this.start();
+        this.writeByte(192);
+        for (var i = 0; i < numsEncoded.length; i++) {
+            this.writeByte(numsEncoded[i]);
+        }
+        this.stop();
+        this.start();
+        this.writeByte(143);
+        this.stop();
+    };
+    return TM1637;
+}());
+exports.TM1637 = TM1637;
+//# sourceMappingURL=tm1637.js.map
