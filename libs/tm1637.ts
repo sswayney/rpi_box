e8494565bcf91b23d39c1a9c2610d7a1030e1685
Shane Swayney <sswayney@conversantmedia.com>
2020-09-11 20:22:13 -0700

    wip

--- libs/tm1637.ts
+++ libs/tm1637.ts
@@ -198,14 +198,14 @@ export class TM1637 {
         this.high(this.pinClk);
     }
     readAck() {
-        // console.log('readAck');
-        this.low(this.pinClk);
-        // this._gpio.setup(this.pinDIO, this._gpio.DIR_IN);
-        this.high(this.pinClk);
-        // const ack = this._gpio.promise.read(this.pinDIO);
-        //this._gpio.setup(this.pinDIO, this._gpio.DIR_OUT);
-        this.low(this.pinClk);
-        // return ack;
+        // // console.log('readAck');
+        // this.low(this.pinClk);
+        // // this._gpio.setup(this.pinDIO, this._gpio.DIR_IN);
+        // this.high(this.pinClk);
+        // // const ack = this._gpio.promise.read(this.pinDIO);
+        // //this._gpio.setup(this.pinDIO, this._gpio.DIR_OUT);
+        // this.low(this.pinClk);
+        // // return ack;
     }
 
     writeByte(byte) {
